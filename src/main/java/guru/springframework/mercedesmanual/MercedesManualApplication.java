89fa7da96772b633a5562eb697ade9b2329e53e1
John Cawelti <caweltjr@hotmail.com>
2025-02-26 10:12:24 -0800

    Force Railway PORT binding

--- src/main/java/guru/springframework/mercedesmanual/MercedesManualApplication.java
+++ src/main/java/guru/springframework/mercedesmanual/MercedesManualApplication.java
@@ -2,12 +2,21 @@ package guru.springframework.mercedesmanual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MercedesManualApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MercedesManualApplication.class, args);
     }
 
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            String port = System.getenv("PORT");
+            factory.setPort(port != null ? Integer.parseInt(port) : 8080);
+        };
+    }
 }
